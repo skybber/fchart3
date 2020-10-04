3101f83f11b7b15cfd776225911a963bee0bb3ed
Vladimir Dvorak <lada.dvorak7@gmail.com>
2020-10-04 11:49:59 +0200

    WidgetMagnitudeScale cleanup

--- fchart3/widget_mag_scale.py
+++ fchart3/widget_mag_scale.py
@@ -17,21 +17,21 @@
 
 import numpy as np
 
-LEGEND_MARGIN = 0.96
-
 class WidgetMagnitudeScale:
 
     def __init__(self, sky_map_engine, field_radius_mm, legend_fontsize, stars_in_scale, lm_stars, star_border_linewidth, legend_linewidth):
         self.engine = sky_map_engine
-        self.field_radius_mm = field_radius_mm
-        self.margin = field_radius_mm*LEGEND_MARGIN
-        self.legend_fontsize =legend_fontsize
+        self.legend_fontsize = legend_fontsize
         self.stars_in_scale = stars_in_scale
         self.lm_stars = lm_stars
         self.star_border_linewidth = star_border_linewidth
         self.legend_linewidth = legend_linewidth
-        self.width = (self.field_radius_mm - self.margin) + 2.3 * self.legend_fontsize
-        self.height = (self.field_radius_mm - self.margin) + (self.stars_in_scale - 0.2) * self.legend_fontsize
+        self.width = self.legend_fontsize
+        if int(self.lm_stars) < 10:
+            self.width += 1.2 * self.legend_fontsize
+        else:
+            self.width += 1.6 * self.legend_fontsize
+        self.height = (self.stars_in_scale + 0.6) * self.legend_fontsize
 
     def get_size(self):
         return (self.width, self.height)
@@ -45,16 +45,15 @@ class WidgetMagnitudeScale:
         fh = self.legend_fontsize
         mags_in_scale = int(self.lm_stars) - np.arange(self.stars_in_scale)
 
-        legendx = -self.margin
-        legendy = -self.margin + np.arange(self.stars_in_scale)*fh
-
+        legendy = bottom + np.arange(self.stars_in_scale)*fh + 0.5*fh
         legendr = self.engine.magnitude_to_radius(mags_in_scale)
+        
         graphics.set_linewidth(self.star_border_linewidth)
 
         for i in range(len(legendy)):
             if mags_in_scale[i] >= -1:
-                self.engine.star(legendx, legendy[i], legendr[i], False)
-                graphics.text_right(legendx + fh*0.75, legendy[i] - fh/3.0, str(mags_in_scale[i]))
+                self.engine.star(left+0.6*fh, legendy[i] + 0.33 * fh, legendr[i], False)
+                graphics.text_left(left+self.width-0.5*fh, legendy[i], str(mags_in_scale[i]))
         graphics.set_linewidth(self.legend_linewidth)
         graphics.line(left, bottom+self.height, left+self.width, bottom+self.height)
         graphics.line(left+self.width, bottom+self.height, left+self.width, bottom)
